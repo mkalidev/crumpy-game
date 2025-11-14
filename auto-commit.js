4b3c2400f009dd0f60a9a92119641fc5269a67e5
mkalidev <greatpannft@gmail.com>
2025-11-14 01:19:39 +0000

    made changes at 2025-11-14T01:19:39.557Z

--- auto-commit.js
+++ auto-commit.js
@@ -23,9 +23,9 @@ const commitChanges = () => {
   }
 };
 
-// Commit immediately, then every 3 seconds
+// Commit immediately, then every 30 seconds
 commitChanges();
-setInterval(commitChanges, 3000);
+setInterval(commitChanges, 30000);
 
-console.log('Auto-commit script running. Committing every 3 seconds...');
+console.log('Auto-commit script running. Committing every 30 seconds...');
 
