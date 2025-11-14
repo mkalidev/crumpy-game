59b75f7f81bd689cfa4c5eb17b4577d9cf352f08
mkalidev <greatpannft@gmail.com>
2025-11-14 01:42:27 +0000

    made changes at 2025-11-14T01:42:27.364Z

---
+++ context/index.js
@@ -0,0 +1,48 @@
+'use client';
+
+import { wagmiAdapter, projectId } from '@/config';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createAppKit } from '@reown/appkit/react';
+import { mainnet, arbitrum } from '@reown/appkit/networks';
+import React, { type ReactNode } from 'react';
+import { cookieToInitialState, WagmiProvider } from 'wagmi';
+
+// Set up queryClient
+const queryClient = new QueryClient();
+
+if (!projectId) {
+  throw new Error('Project ID is not defined');
+}
+
+// Set up metadata
+const metadata = {
+  name: '2405 Game',
+  description: 'Play 2405 Game and earn points with your Web3 wallet',
+  url: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000',
+  icons: ['https://avatars.githubusercontent.com/u/179229932'],
+};
+
+// Create the modal
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks: [mainnet, arbitrum],
+  defaultNetwork: mainnet,
+  metadata: metadata,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+
+function ContextProvider({ children, cookies }) {
+  const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig, cookies);
+
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default ContextProvider;
+
