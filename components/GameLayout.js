a0302378d61aaac2727756fdf20ecbc57d2db3f7
mkalidev <greatpannft@gmail.com>
2025-11-14 01:58:08 +0000

    made changes at 2025-11-14T01:58:07.178Z

--- components/GameLayout.js
+++ components/GameLayout.js
@@ -3,9 +3,11 @@ import Leaderboard from './Leaderboard';
 
 export default function GameLayout({ onGameScore }) {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-[1fr_350px] gap-6 lg:gap-8">
+    <div className="flex flex-col gap-6 lg:gap-8">
       <Game2048 onScore={onGameScore} />
-      <Leaderboard />
+      <div className="max-w-[500px] mx-auto w-full">
+        <Leaderboard />
+      </div>
     </div>
   );
 }
